db0e2e7bf895309fc6796457649611c97ea6ced5
Cecilia <earwen1986@gmail.com>
2021-02-25 14:40:06 +0000

    completed strictEqual exercise

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -50,19 +50,19 @@ suite("Unit Tests", function () {
     });
     // #6
     test("#strictEqual, #notStrictEqual", function () {
-      assert.fail(6, "6");
-      assert.fail(6, 3 * 2);
-      assert.fail(6 * "2", 12);
-      assert.fail([1, "a", {}], [1, "a", {}]);
+      assert.notStrictEqual(6, "6");
+      assert.strictEqual(6, 3 * 2);
+      assert.strictEqual(6 * "2", 12);
+      assert.notStrictEqual([1, "a", {}], [1, "a", {}]);
     });
     // #7
     test("#deepEqual, #notDeepEqual", function () {
-      assert.fail(
+      assert.deepEqual(
         { a: "1", b: 5 },
         { b: 5, a: "1" },
         "keys order doesn't matter"
       );
-      assert.fail(
+      assert.notDeepEqual(
         { a: [5, 6] },
         { a: [6, 5] },
         "array elements position does matter !!"
